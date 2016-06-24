bd66871b874e37520e297e84caca70f9e8f4c42d
Vladimir Shcherbakov <vlashch@microsoft.com>
2016-06-24 16:46:50 -0700

    [Eclipse] Login dialog: SWT Shell is replaced with SWT Dialog

--- PluginsAndFeatures/com.microsoftopentechnologies.wacommon/src/com/microsoftopentechnologies/wacommon/adauth/SwtBrowserWIndow.java
+++ PluginsAndFeatures/com.microsoftopentechnologies.wacommon/src/com/microsoftopentechnologies/wacommon/adauth/SwtBrowserWIndow.java
@@ -5,11 +5,16 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.LocationAdapter;
 import org.eclipse.swt.browser.LocationEvent;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -32,7 +37,7 @@ public class SwtBrowserWIndow implements com.microsoft.auth.IWebUi {
 	@Override
 	public Future<String> authenticateAsync(URI requestUri, URI redirectUri) {
 		
-		//System.out.println("==> run authenticateAsync ---------------");
+		System.out.println("==> run authenticateAsync ---------------");
 		
 		final String redirectUriStr = redirectUri.toString();
 		final String requestUriStr = requestUri.toString();
@@ -41,49 +46,17 @@ public class SwtBrowserWIndow implements com.microsoft.auth.IWebUi {
 		System.out.println("==> requestUriStr: " + requestUriStr);
 		
 		try {
-			
 			final Runnable gui = new Runnable() {
 				@Override
 				public void run() {
 					
 					try {
-						//System.out.println("==> run gui ---------------");
+						System.out.println("==> run gui ---------------");
 				        Display display = Display.getDefault();
 				        final Shell activeShell = display.getActiveShell();
-				        final Shell shell = new Shell(activeShell, SWT.APPLICATION_MODAL | SWT.CLOSE | SWT.TITLE | SWT.BORDER);
-				        shell.setLayout(new FillLayout());
-				        final int HEIGHT = 700;
-				        final int WIDTH = 500;
-				        shell.setSize(WIDTH, HEIGHT);
-				        shell.setActive(); 
-				        
-				        Browser.clearSessions();
-				        final Browser browser = new Browser(shell, SWT.NONE);
-				        
-				        browser.addLocationListener(new LocationAdapter() {
-				            @Override
-				            public void changing(LocationEvent locationEvent) {
-				            	//System.out.println("==> locationEvent.location: " + locationEvent.location);
-				                if(locationEvent.location.startsWith(redirectUriStr)) {
-				                	setResult(locationEvent.location);
-				                	Display.getDefault().asyncExec(new Runnable() {
-										@Override
-										public void run() {
-											//System.out.println("==> shell.close() ---------------");
-											browser.stop();
-						                	shell.close();
-										}
-				                	});
-				                }
-				            }
-				        });
-				        
-				        browser.setUrl(requestUriStr);
-				        shell.open();
-				        while (shell != null && !shell.isDisposed()) {
-				            if (!display.readAndDispatch())
-				            	display.sleep ();
-				        }
+						LoginDialog dlg = new LoginDialog(activeShell, redirectUriStr, requestUriStr);
+						dlg.open();
+						setResult(dlg.getResult());
 				        
 					} catch (Exception e) {
 						e.printStackTrace();
@@ -99,6 +72,7 @@ public class SwtBrowserWIndow implements com.microsoft.auth.IWebUi {
 	        };
        
 	        Display.getDefault().syncExec(gui);
+	        
 	        // just to return future to comply interface
 	        return Executors.newSingleThreadExecutor().submit(worker);
 	        
@@ -108,5 +82,90 @@ public class SwtBrowserWIndow implements com.microsoft.auth.IWebUi {
 		
 		return null;
 	}
+}
 
+class LoginDialog extends Dialog {
+	
+	final String redirectUriStr;
+	final String requestUriStr;
+	
+	private String res = null;
+	
+	private void setResult(String res) {
+		this.res = res;
+	}
+	
+	public String getResult() {
+		return res;
+	}
+
+	public LoginDialog(Shell parentShell, String redirectUri, String requestUri) {
+		super(parentShell);
+		this.redirectUriStr = redirectUri;
+		this.requestUriStr = requestUri;
+
+	}
+	
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Azure Login Dialog");
+	}
+	
+	@Override
+	protected Control createButtonBar(Composite parent) {
+		Control ctrl = super.createButtonBar(parent);
+		return ctrl;
+	}
+	
+	@Override
+	protected void createButtonsForButtonBar(final Composite parent) { 
+	  GridLayout layout = (GridLayout)parent.getLayout();
+	  layout.marginHeight = 0;
+	}
+	
+	@Override
+	protected Point getInitialSize() {
+	    return new Point(500, 750);
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent) {
+
+		Composite container = (Composite) super.createDialogArea(parent);
+		
+		FillLayout fillLayout = new FillLayout();
+		fillLayout.type = SWT.VERTICAL;
+		container.setLayout(fillLayout);
+		
+		createDlgBody(container);
+		return container;
+	}
+	
+	private void createDlgBody(Composite container) {
+        final Browser browser = new Browser(container, SWT.NONE);
+
+        browser.addLocationListener(new LocationAdapter() {
+            @Override
+            public void changing(LocationEvent locationEvent) {
+            	System.out.println("==> locationEvent.location: " + locationEvent.location);
+                if(locationEvent.location.startsWith(redirectUriStr)) {
+                	setResult(locationEvent.location);
+                	Display.getDefault().asyncExec(new Runnable() {
+						@Override
+						public void run() {
+							Browser.clearSessions();
+		                	close();
+						}
+                	});
+                }
+            }
+        });
+
+        String[] headers = new String[] {
+        		"User-Agent: SwtBrowser"
+        };
+        
+        browser.setUrl(requestUriStr, null, headers);
+	}
 }
