d8bbe3604e5b262a92daf8cf73b98c0a77cbdce6
Vladimir Shcherbakov <vlashch@microsoft.com>
2016-09-25 11:29:20 -0700

    issue #180: rewrote for Eclipse

--- PluginsAndFeatures/com.microsoft.webapp/src/com/microsoft/webapp/util/WebAppUtils.java
+++ PluginsAndFeatures/com.microsoft.webapp/src/com/microsoft/webapp/util/WebAppUtils.java
@@ -52,14 +52,54 @@ public class WebAppUtils {
 	}
 
 	// HTTP GET request
-	public static void sendGet(String sitePath) throws Exception {
-		URL url = new URL(sitePath);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", "AzureToolkit for Eclipse");
-		con.getResponseCode();
-	}
-	
+    public static int sendGet(String sitePath) throws Exception {
+        URL url = new URL(sitePath);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "AzureToolkit for Intellij");
+        return con.getResponseCode();
+    }
+
+    public static void checkSiteIsUp(String siteUrl) throws Exception {
+        for (int i=0; i<5; ++i) {
+            //System.out.println("==> Sending get to " + siteUrl + "...");
+            int statusCode = sendGet(siteUrl);
+            System.out.println("\t status code is " + statusCode);
+            if (statusCode < 400) break;
+            //System.out.println("\t Sleeping 5 sec...");
+            Thread.sleep(5000);
+        }
+    }
+    public static void checkSiteIsDown(String siteUrl) throws Exception {
+        for (int i=0; i<5; ++i) {
+            System.out.println("==> Sending get to " + siteUrl + "...");
+            int statusCode = sendGet(siteUrl);
+            System.out.println("\t status code is " + statusCode);
+            if (statusCode >= 400) break;
+            //System.out.println("\t Sleeping 5 sec...");
+            Thread.sleep(5000);
+        }
+    }
+
+    public static boolean isRemoteFileExist(FTPClient ftp, String fileName) throws IOException {
+        FTPFile[] files = ftp.listFiles("/site/wwwroot");
+        for (FTPFile file : files) {
+            if (file.isFile() && file.getName().equalsIgnoreCase(fileName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRemoteDirExist(FTPClient ftp, String fileName) throws IOException {
+        FTPFile[] files = ftp.listFiles("/site/wwwroot");
+        for (FTPFile file : files) {
+            if (file.isDirectory() && file.getName().equalsIgnoreCase(fileName)) {
+                return true;
+            }
+        }
+        return false;
+    }	
 	public static void openDebugLaunchDialog(Object toSelect) {
 		ILaunchGroup[] grp = DebugUITools.getLaunchGroups();
 		DebugUITools.openLaunchConfigurationDialogOnGroup(PluginUtil.getParentShell(),
